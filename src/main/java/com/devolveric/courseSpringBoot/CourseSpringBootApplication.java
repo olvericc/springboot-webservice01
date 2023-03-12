ec7702fb1c1de71a65f6ff5ed2d22318ffe78260
olvericc <olv.ericlopes@gmail.com>
2023-03-12 00:08:09 -0300

    project created

---
+++ src/main/java/com/devolveric/courseSpringBoot/CourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.devolveric.courseSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devolveric/courseSpringBoot/CourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devolveric.courseSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
